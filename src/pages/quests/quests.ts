1b8d0a68789ea52da72094b498e4d5918535dd85
Alicia Lombarte <alicia.lombarte.garcia@gmail.com>
2019-03-23 18:49:49 +0100

    Add logout feature

--- src/pages/quests/quests.ts
+++ src/pages/quests/quests.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { CameraPage } from '../camera/camera';
 import { QuestionnairePage } from '../questionnaire/questionnaire';
 import { AuthService } from '../../providers/auth-service/auth-service';
+import { LoginPage } from '../login/login';
 
 @IonicPage()
 @Component({
@@ -18,7 +19,7 @@ export class QuestsPage {
   userData = { email:''};
   seguimiento: String = null;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public authService: AuthService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public authService: AuthService,  private toastCtrl: ToastController) {
     this.userData.email = localStorage.getItem('email');
   }
 
@@ -65,9 +66,50 @@ export class QuestsPage {
     });
   }
 
-
   woundTracking() {
     this.navCtrl.push(CameraPage);
   }
+  
+  presentToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 2000,
+      position: 'bottom',
+      //dismissOnPageChange: true
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
+  }
+  presentToastError(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 4000,
+      position: 'bottom',
+      //dismissOnPageChange: true
+    });
+}
+logout(){
+  this.authService.logout()
+  .then(data => {
+    this.data = data;
+    console.log(this.data._body);
+    if(this.data._body=="{\"result\":200,\"listUsers\":null}"){
+      localStorage.clear();
+      this.navCtrl.setRoot(LoginPage);
+      this.presentToast("Sesión finalizada correctamente.");
+    } else{
+      this.presentToastError("Error al finalizar la sesión.");
+      this.navCtrl.setRoot(QuestsPage);
+    }
+  },
+  (error) => {
+    console.log(error);
+    this.presentToastError("Error en inicio de sesión. Sin conexión.");
+  });
+};
 
 }
